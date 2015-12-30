df866ec61a39d696052671dfd0f97d83ad3805c2
Nick Touchette <touchette@gmail.com>
2015-12-30 04:49:46 -0500

    fixed random number issue + refactored button ids

--- js/app.js
+++ js/app.js
@@ -90,14 +90,13 @@ $(document).ready(function() {
   init();
 
   function Square(index) {
-    this.index = index;
+    this.number = index;
     this.isSelected = false;
-    this.element = `.board ul li:nth-child(${this.index + 1}) button`;
+    this.element = `.board ul li:nth-child(${this.number}) button`;
     this.isHit = false;
 
     // create button in DOM using jquery
-    $(".board ul").append(`<li><button btn-id="${this.index}">${this.index + 1}</button></li>`);
-
+    $(".board ul").append(`<li><button btn-id="${this.number}">${this.number}</button></li>`);
     // highlight a square and detect if square is selected
     this.highlight = function() {
       $(this.element).css("color", "rgb(255,255,255)");
@@ -123,7 +122,7 @@ $(document).ready(function() {
       if (this.isHit) {
         $(this.element).addClass("blink");
       }
-    }
+    };
 
     // Select isSelected and iterate selfRefresh.selectedCount, style accordingly
     this.select = function() {
@@ -146,9 +145,9 @@ $(document).ready(function() {
         $(this.element).removeClass("blink");
       }
       $(this.element).css("color", "initial");
-      $(this.element).text(this.index + 1);
+      $(this.element).text(this.number);
     };
-  }
+  };
 
   // Create button objects and display board
   function init() {
@@ -164,7 +163,7 @@ $(document).ready(function() {
 
     $('#speed').text(`Speed ${ 400 / drawSpeed }X`);
 
-    for (var i = 0; i < 80; i++) {
+    for (var i = 1; i <= 80; i++) {
       // create Square object with values 1 to 80 and push to board
       board.push(new Square(i));
     }
@@ -173,14 +172,14 @@ $(document).ready(function() {
   // Randomly generate 20 unique numbers and return the array
   function generateNumbers() {
     var randomNumberArray = [],
-      availableNumbers = [];
+        availableNumbers = [];
 
     for (var i = 1; i <= 80; i++) {
       availableNumbers.push(i);
     }
 
     for (var i = 0; i < 20; i++) {
-      var randomNumber = Math.floor(Math.random() * (availableNumbers.length - 1));
+      var randomNumber = Math.floor(Math.random() * (availableNumbers.length));
       randomNumberArray.push(Number(availableNumbers.splice(randomNumber, 1)));
     }
     return randomNumberArray;
@@ -215,7 +214,7 @@ $(document).ready(function() {
       }
 
       function highlight() {
-        board[randomNumbers[count]].highlight();
+        board[randomNumbers[count] - 1].highlight();
         if (count === randomNumbers.length - 1) {
           calculatePayout();
           idleState = true;
@@ -310,12 +309,12 @@ $(document).ready(function() {
   // Button Events
   $('.board li button').on({
     mousedown: function() {
-      board[$(this).attr("btn-id")].select();
+      board[$(this).attr("btn-id") - 1].select();
       mouseDown = true;
     },
     mouseover: function() {
       if (mouseDown)
-        board[$(this).attr("btn-id")].select();
+        board[$(this).attr("btn-id") - 1].select();
     }
   });
 
